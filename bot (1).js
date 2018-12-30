4a5a1b9f4673da6aee517af85a16d8531b563488
ViiRo <45905915+ViiRo@users.noreply.github.com>
2018-12-30 05:56:24 -0800

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,8 @@
+﻿client.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("528217884435021844");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**__#.Welcome To ~Stõrm , Ňetwørķ..1k__** :gifHeart: `), 4000)        
+}
+});
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
